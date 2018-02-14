98c4c427133e208f0f770f4fd778fe580c9b0f2a
Alex Cruz <du.alexcruz@gmail.com>
2018-02-14 12:58:17 -0500

    Organize overlays / themes
    Someone suggested this a little while ago but I just never got around
    to doing it until now. This keeps everything oranigzed and ready.
    
    Change-Id: I51021ec22e04aa48f4f2ba0ef58b8bb9a59d9df1

--- config/packages.mk
+++ config/packages.mk
@@ -34,6 +34,13 @@ PRODUCT_PACKAGES += \
     SettingsDarkTheme \
     SystemDarkTheme
 
+# Overlays
+PRODUCT_PACKAGES += \
+    DuiDark \
+    GBoardDark \
+    SettingsDark \
+    SystemDark
+
 # Extra tools
 PRODUCT_PACKAGES += \
     7z \
