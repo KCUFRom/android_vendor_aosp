7b500eb198019c620897e463c35738eeaf2b1eef
phenomenall <venkysaihot@gmail.com>
2019-09-10 03:49:04 +0200

    remove vendorsetup.sh

--- vendorsetup.sh
+++
@@ -1,4 +0,0 @@
-for combo in $(curl -s https://raw.githubusercontent.com/KCUFRom/android_vendor_kcuf/p/kcuf.devices | sed -e 's/#.*$//' | awk '{printf "kcuf_%s-%s\n", $1, $2}')
-do
-    add_lunch_combo $combo
-done
